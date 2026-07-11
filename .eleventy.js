3e644c7354de661d23883db8a47fc6fedac0763e
FRSBE53 <rosivanbezerra@bezerraesilvaadvocacia.adv.br>
2026-07-11 18:11:44 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("uploads");
+  eleventyConfig.addPassthroughCopy("admin");
 
   return {
     dir: {
